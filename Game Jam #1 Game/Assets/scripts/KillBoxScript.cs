b441a17d1d337535e60fbfcf1870a4dac241c8bc
kingcosmo-hash <agrant44@asu.edu>
2026-02-09 22:11:30 -0700

    Got Player to teleport to start
    The player teleports to the start of the level if they fall off the map. Also made the player respawn and kill box invisible

--- Game Jam #1 Game/Assets/scripts/KillBoxScript.cs
+++ Game Jam #1 Game/Assets/scripts/KillBoxScript.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class KillBoxScript : MonoBehaviour
 {
+    [SerializeField] public GameObject respawnPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-
+            other.gameObject.transform.position = respawnPoint.transform.position;
         }
     }
 
